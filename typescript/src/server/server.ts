ec1f72dc67f07260566a5c946375acd2f7c9b732
mod <modularizer@gmail.com>
2026-04-04 22:16:03 -0700

    release: 0.3.0
    browser python client side

--- typescript/src/server/server.ts
+++ typescript/src/server/server.ts
@@ -1288,7 +1288,7 @@ export class PLATServer {
             openapi: '3.1.0',
             info: {
                 title: 'plat API',
-                version: '0.2.0',
+                version: '0.3.0',
             },
             paths,
         }
