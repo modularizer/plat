e293c7b1e981b8a779bdd43cd0c20476a640fad3
mod <modularizer@gmail.com>
2026-04-13 23:00:10 -0700

    release: 0.11.0
    authority

--- typescript/src/client-side-server/server.ts
+++ typescript/src/client-side-server/server.ts
@@ -711,7 +711,7 @@ export class PLATClientSideServer {
       openapi: '3.1.0',
       info: this.options.openapiInfo ?? {
         title: 'plat client-side server',
-        version: '0.10.0',
+        version: '0.11.0',
       },
       paths,
     }
